99da76114466cacc0da5d0f39bc61ec23e70fc8b
Jettsyy <teazyyy2512@gmail.com>
2025-04-03 14:47:53 +0700

    feat: change base URL

--- src/utils/instanceAxios.ts
+++ src/utils/instanceAxios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instanceAxios = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
